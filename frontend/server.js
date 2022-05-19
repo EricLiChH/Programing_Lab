3f3f33951f7a156714ea8ab9bdebbd12a667d559
Eric LiCH <Eric_LiCH@outlook.com>
2022-05-19 22:09:56 +0800

    ”combination“

---
+++ frontend/server.js
@@ -0,0 +1,32 @@
+const Koa = require('koa'),
+      route = require('koa-route'),
+      websockify = require('koa-websocket'),
+      http = require('http'),
+      app = websockify(new Koa());
+
+app.ws.use(route.all('/', ctx => {
+    // websocket作为“ctx.websocket”添加到上下文中。
+    ctx.websocket.on('message', message => {
+        startRequest(message, ctx);
+    });
+}));
+
+function startRequest(message, ctx) {
+    const net = require('net');
+    const port = 8001;
+    const hostname = '127.0.0.1';
+    const sock = new net.Socket();
+    sock.setEncoding = 'UTF-8'
+    sock.connect(port, hostname, function(){
+        sock.write(message)
+    });
+	sock.on('data', function(res){
+        ctx.websocket.send(res.toString());
+    });
+}
+
+// 监听端口、启动程序
+app.listen(3000, err => {
+    if (err) throw err;
+    console.log('服务已启动');
+})
